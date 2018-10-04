a61aa13a8fcc8fe2186bc567636d7459421d33fb
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-03 22:13:14 -0500

    P/P03 finished

--- psets/P03/Practica3.hs
+++ psets/P03/Practica3.hs
@@ -6,20 +6,26 @@ import Binario
 binarios :: [Int] -> [Binario]
 binarios l = map natToBin l
 
---Ejercicio 2.2
+-- Ejercicio 2.2
 pares :: [Binario] -> [Binario]
 pares l = filter (\b -> case b of (Cero _) -> True; _ -> False) l
 
---Ejercicio 2.3
--- tooLong :: [String] -> [String]
--- tooLong l = filter (length > )
+-- Ejercicio 2.3
+tooLong :: [String] -> [String]
+tooLong l = filter (\x -> length x <= 7) l
 
--- --Ejercicio 2.4
---  sFibonacci :: Int -> [Int]
---  --Tu código va aquí
---  sFibonacci = error "Borrar esta linea"
+-- Ejercicio 2.4
+sFibonacci :: Int -> [Int]
+sFibonacci n = map (fibonacci) [0..n]
 
--- --Ejercicio 2.5
---  quitaElemento :: (Eq a) => [a] -> a -> [a]
---  --Tu código va aquí
---  quitaElemeto = error "Borrar esta linea"
+-- Ejercicio 2.5
+quitaElemento :: (Eq a) => [a] -> a -> [a]
+quitaElemento l e = filter (/=e) l
+
+-- | Auxiliary functions
+
+-- Fibonacci
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = (fibonacci $ n - 1) + (fibonacci $ n - 2)
