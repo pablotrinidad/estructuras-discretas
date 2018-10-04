a37d9d6de7bada229544feeac119b5247f13fe05
Pablo Trinidad <pabloatp0204@gmail.com>
2018-10-03 21:21:25 -0500

    2.2 P/P03 finished

--- psets/P03/Practica3.hs
+++ psets/P03/Practica3.hs
@@ -2,27 +2,24 @@ module Practica3 where
 
 import Binario
 
---Ejercicio 2.1
- binarios :: [Int] -> [Binario]
- --Tu código va aquí
- binarios = error "Borrar esta linea"
+-- Ejercicio 2.1
+binarios :: [Int] -> [Binario]
+binarios l = map natToBin l
 
 --Ejercicio 2.2
- pares :: [Binario] -> [Binario]
- --Tu código va aquí
- pares = error "Borrar esta linea"
+pares :: [Binario] -> [Binario]
+pares l = filter (\b -> case b of (Cero _) -> True; _ -> False) l
 
 --Ejercicio 2.3
- tooLong :: [String] -> [String]
- --Tu código va aquí
- tooLong = error "Borrar esta linea"
+-- tooLong :: [String] -> [String]
+-- tooLong l = filter (length > )
 
---Ejercicio 2.4
- sFibonacci :: Int -> [Int]
- --Tu código va aquí
- sFibonacci = error "Borrar esta linea"
+-- --Ejercicio 2.4
+--  sFibonacci :: Int -> [Int]
+--  --Tu código va aquí
+--  sFibonacci = error "Borrar esta linea"
 
---Ejercicio 2.5
- quitaElemento :: (Eq a) => [a] -> a -> [a]
- --Tu código va aquí
- quitaElemeto = error "Borrar esta linea"
+-- --Ejercicio 2.5
+--  quitaElemento :: (Eq a) => [a] -> a -> [a]
+--  --Tu código va aquí
+--  quitaElemeto = error "Borrar esta linea"
